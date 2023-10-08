c0c9c96e95975070b3aa4be37f80c58667f3cad1
gadaradarsh <145762122+gadaradarsh@users.noreply.github.com>
2023-10-08 20:48:15 +0530

    Update SecondMax.c

--- Arrays/SecondMax.c
+++ Arrays/SecondMax.c
@@ -1,15 +1,25 @@
-#include<stdio.h>
-int main(){
-    int max=-1,i,smax=-1;// #include<limit> nathi karvu 
-    int arr[5]={1,3,4,6,5};
-   for (i=0;i<5;i++)
-   { if (max<arr[i]) {
-    max=arr[i];
-   }
-   for (i=0;i<5;i++)
-   { if (smax<arr[i]&&smax<max) {
-    smax=arr[i];
-   }
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int max = -1, smax = INT_MIN; // Initialize smax with INT_MIN
+    int arr[5] = {1, 3, 4, 6, 5};
     
-   } printf("%d",smax);
+    // Calculate max
+    for (int i = 0; i < 5; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+
+    // Calculate smax
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] < max && arr[i] > smax) {
+            smax = arr[i];
+        }
+    }
+
+    printf("%d", smax);
+
+    return 0;
 }
